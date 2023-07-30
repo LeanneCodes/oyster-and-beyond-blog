391a5a5769620b99d47880249a5f0e6f3f17b886
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-07-30 20:53:05 +0100

    handle errors from vercel

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import moment from 'moment';
 import Link from 'next/link'
+import Image from 'next/image'
 
 const PostCard = ({ post }) => {
   console.log(post)
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 mb-8'>
         <div className='relative overflow-hidden shadow-md pb-8 mb-6'>
-          <img
+          <Image
             src={post.featuredImage.url}
             alt={post.title} 
             className='object-top relative h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
@@ -17,7 +18,7 @@ const PostCard = ({ post }) => {
           </h1>
           <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
             <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-              <img
+              <Image
                 src={post.author.photo.url}
                 alt={post.author.name}
                 height="30px"
